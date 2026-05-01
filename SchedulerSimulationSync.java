614a157b2a470409c38488c2f0031c941ace8c68
Farah66x <444051798@std.psau.edu.sa>
2026-05-02 01:30:21 +0300

    Add semaphore to control CPU access

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
@@ -44,6 +45,8 @@ class SharedResources {
     // Example: public static final ReentrantLock lock = new ReentrantLock();
 
     // TODO #2: Add a Semaphore to limit concurrent process execution
+    // Added semaphore for CPU control
+    public static final Semaphore cpuSemaphore = new Semaphore(1);
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
 
     // Method to increment context switch counter
